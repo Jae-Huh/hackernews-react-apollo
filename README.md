c6a372a5e8cb63820ef0bf30c80cb538aef664a2
Jae Huh <jaeeunhuh@gmail.com>
2017-08-31 14:56:56 +1200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## hackernews-react-apollo
+
+Hackernews clone created using React, GraphQL and Apollo.
+https://www.howtographql.com/react-apollo/0-introduction/
---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/App'
+import registerServiceWorker from './registerServiceWorker'
+import './styles/index.css'
+// 1
+import { ApolloProvider, createNetworkInterface, ApolloClient } from 'react-apollo'
+
+// 2
+const networkInterface = createNetworkInterface({
+  uri: 'https://api.graph.cool/simple/v1/cj6zsy76w0ewv0197w6fj1crk'
+  // Can get the uri from running 'graphcool endpoints' on command line or from graphcool console.
+})
+
+// 3
+const client = new ApolloClient({
+  networkInterface
+})
+
+// 4
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+  , document.getElementById('root')
+)
+registerServiceWorker()
