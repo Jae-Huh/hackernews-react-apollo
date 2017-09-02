84aff798424956d3169b0a44ae7a41a0c09c1a68
Jae Huh <jaeeunhuh@gmail.com>
2017-09-02 17:54:49 +1200

    Show updates instantaneously using graphql subscriptions - websocket error

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 // 1
 import { ApolloProvider, createNetworkInterface, ApolloClient } from 'react-apollo'
-import { GC_AUTH_TOKEN } from './constants'
+import { SubscriptionClient, addGraphQLSubscriptions } from 'subscriptions-transport-ws'
 
+import { GC_AUTH_TOKEN } from './constants'
 import App from './components/App'
 import registerServiceWorker from './registerServiceWorker'
 import './styles/index.css'
@@ -14,6 +15,18 @@ const networkInterface = createNetworkInterface({
   uri: 'https://api.graph.cool/simple/v1/cj6zsy76w0ewv0197w6fj1crk'
   // Can get the uri from running 'graphcool endpoints' on command line or from graphcool console.
 })
+
+const wsClient = new SubscriptionClient('wss://subscriptions.graph.cool/v1/cj6zsy76w0ewv0197w6fj1crk', {
+  reconnect: true,
+  connectionParams: {
+    authToken: localStorage.getItem(GC_AUTH_TOKEN),
+  }
+})
+
+const networkInterfaceWithSubscriptions = addGraphQLSubscriptions(
+  networkInterface,
+  wsClient
+)
 
 networkInterface.use([{
   applyMiddleware(req, next) {
@@ -26,9 +39,8 @@ networkInterface.use([{
   }
 }])
 
-// 3
 const client = new ApolloClient({
-  networkInterface
+  networkInterface: networkInterfaceWithSubscriptions
 })
 
 // 4
